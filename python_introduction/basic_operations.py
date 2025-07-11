ba2904eda15e654705ffda925727433cd1863b15
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-07-11 07:48:13 +0300

    Added python introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 3
+number2 = 10
+
+sum = number1 + number2
+difference = number2 - number1
+product = number1 * number2
+
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number2, "and", number1, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
