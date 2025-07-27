0274a64de4316a592c61184d4bfc32c6000c44e2
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-07-27 18:25:33 +0300

    Fixed some typos and also added exception handling for value error on temperature input

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -4,17 +4,21 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-def convert_to_fahrenheit(celsuis):
-    return (celsuis * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+def convert_to_fahrenheit(celsius):
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-temp = float(input("Enter the temperature to convert: "))
+try: 
+    temp = float(input("Enter the temperature to convert: "))
+except (ValueError, TypeError):
+    print("Invalid temperature. Please enter a numeric value.")
+    exit()
 cel_or_fah = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
-if cel_or_fah == "C" and temp != None:
+if cel_or_fah == "C":
     fah = convert_to_fahrenheit(temp)
-    print(f"{temp} degress celsius is {fah} degress fahrenheit.")
-elif cel_or_fah == "F" and temp != None:
+    print(f"{temp} degrees celsius is {fah} degress fahrenheit.")
+elif cel_or_fah == "F":
     cel = convert_to_celsius(temp)
-    print(f"{temp} degress fahrenheit is {cel} degress celsius.")
+    print(f"{temp} degrees fahrenheit is {cel} degress celsius.")
 else:
-    print("Please provide the necessary inputs for both temperature and unit.")
+    print("Please provide the necessary input for unit.")
