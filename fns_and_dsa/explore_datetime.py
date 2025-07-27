62a58b58e8e2099b0513d7dd7a0d062001c05ff2
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-07-27 15:30:33 +0300

    Added the datetime script

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+display_current_datetime()
+
+number_of_days = int(input("Enter the number of days: "))
+def calculate_future_date(days):
+    future_date = datetime.now() + timedelta(days=days)
+    print(future_date.strftime("%Y-%m-%d"))
+
+calculate_future_date(number_of_days)
