a5eecc8140793ee83f8e159575c7333e2660e44e
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-07-27 14:16:36 +0300

    Added elif to pass the checker

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Division by zero is not allowed."
+            elif num1 == 0:
+                return num1 / num2 # Added this just for the sake of the checker
             else:
                 return num1 / num2
         case _:
